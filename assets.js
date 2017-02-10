bf2669eb3a722be73b427231e2d2a009f601ce97
Matteo Collina <hello@matteocollina.com>
2017-02-10 12:14:38 +0100

    Updated

--- assets.js
+++ assets.js
@@ -7,29 +7,26 @@ var defaults = {
   port: 8989
 }
 
-function assetsService (opts) {
+function assetsService (opts, cb) {
   opts = xtend(defaults, opts)
 
   var server = new Hapi.Server()
 
   server.connection({ port: opts.port })
 
-  function hello (request, reply) {
-    reply('Hello World')
-  }
-
-  server.route({ method: 'GET', path: '/', handler: hello })
+  server.register(require('./lib/assets'), (err) => {
+    cb(err, server)
+  })
 
   return server
 }
 
 function start (opts) {
-  var server = assetsService(opts)
-  server.start(function (err) {
+  assetsService(opts, (err, server) => {
     if (err) { throw err }
-
-    server.connections.forEach(function (conn) {
-      console.log('server started on port', conn.settings.port)
+    server.start((err) => {
+      if (err) { throw err }
+      console.log('Server running at:', server.info.uri)
     })
   })
 }
@@ -41,6 +38,9 @@ if (require.main === module) {
     integer: 'port',
     alias: {
       'port': 'p'
+    },
+    default: {
+      port: 3000
     }
   }))
 }
---
+++ test/api.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const code = require('code')
+const Lab = require('lab')
+const lab = exports.lab = Lab.script({ output: process.stdout })
+const assetsService = require('../')
+
+lab.experiment('Hello', function () {
+  let server
+  lab.beforeEach((done) => {
+    assetsService({ port: 8989 }, (err, s) => {
+      server = s
+      done(err)
+    })
+  })
+
+  lab.test('Testing for "Hello World"', function (done) {
+    const options = { method: 'GET', url: '/' }
+    server.inject(options, function (response) {
+      const result = response.result
+      code.expect(result).to.equal('Hello World')
+      done()
+    })
+  })
+})
