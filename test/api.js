c55430b423c2d22b58613590cef8198f7c5579b3
Matteo Collina <hello@matteocollina.com>
2017-12-03 11:35:46 +0100

    Basic integration

--- test/api.js
+++ test/api.js
@@ -1,28 +1,75 @@
 'use strict'
 
-const tap = require('tap')
 const Fastify = require('fastify')
-const { beforeEach, afterEach, test } = tap
+const {
+  beforeEach,
+  afterEach,
+  tearDown,
+  test
+} = require('tap')
 const assetsService = require('../')
 const connString = 'postgres://localhost/assets_tests'
+const Assets = require('@matteo.collina/assets-pg')
+const db = Assets(connString)
 
 let server
 beforeEach((done) => {
-  server = Fastify()
-  server.register(assetsService, {
-    connString
+  db.dropSchema(() => {
+    // ignore errors
+    db.createSchema((err) => {
+      if (err) {
+        return done(err)
+      }
+
+      server = Fastify()
+      server.register(assetsService, {
+        connString
+      })
+      server.ready(done)
+    })
   })
-  server.ready(done)
 })
 
 afterEach((done) => {
   server.close(done)
 })
 
-test('Hello', function (t) {
-  const options = { method: 'GET', url: '/' }
-  server.inject(options, function (response) {
-    t.equal(response.payload, 'Hello World')
+tearDown(db.end)
+
+test('POST an asset', function (t) {
+  const asset = {
+    name: 'my long asset',
+    status: 'wait'
+  }
+  server.inject({
+    method: 'POST',
+    url: '/',
+    payload: asset
+  }, function (response) {
+    t.equal(response.statusCode, 201)
+    t.ok(response.headers['location'])
+    // the ID matches the one in the locaiton header
+    asset.id = Number(response.headers['location'].slice(1))
+    t.deepEqual(JSON.parse(response.payload), asset)
     t.end()
+  })
+})
+
+test('GET an asset', function (t) {
+  const asset = {
+    name: 'my long asset',
+    status: 'wait'
+  }
+  db.put(asset, (err, asset) => {
+    t.error(err)
+
+    server.inject({
+      method: 'GET',
+      url: `/${asset.id}`
+    }, function (response) {
+      t.equal(response.statusCode, 200)
+      t.deepEqual(JSON.parse(response.payload), asset)
+      t.end()
+    })
   })
 })
